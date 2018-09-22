5d5297b4c930725098d1a2dc769dcfdcf113bab8
robbert <robbert@vdzon.com>
2018-09-22 09:09:36 +0200

    add lombok

--- src/main/java/my/service/SmokerSession.java
+++ src/main/java/my/service/SmokerSession.java
@@ -2,46 +2,27 @@ package my.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @DynamoDBTable(tableName = "smokersessions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class SmokerSession {
+    @DynamoDBHashKey
     private String sessionDateTime;
     private long lastUpdate;
     List<Temperature> temperatures = new ArrayList<>();
-
-    public SmokerSession() {
-    }
 
     public SmokerSession(String sessionDateTime, long lastUpdate) {
         this.sessionDateTime = sessionDateTime;
         this.lastUpdate = lastUpdate;
     }
 
-    @DynamoDBHashKey
-    public String getSessionDateTime() {
-        return sessionDateTime;
-    }
-
-    public void setSessionDateTime(String sessionDateTime) {
-        this.sessionDateTime = sessionDateTime;
-    }
-
-    public long getLastUpdate() {
-        return lastUpdate;
-    }
-
-    public void setLastUpdate(long lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
-
-    public List<Temperature> getTemperatures() {
-        return temperatures;
-    }
-
-    public void setTemperatures(List<Temperature> temperatures) {
-        this.temperatures = temperatures;
-    }
 }
--- src/main/java/my/service/Temperature.java
+++ src/main/java/my/service/Temperature.java
@@ -1,8 +1,14 @@
 package my.service;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @DynamoDBDocument
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Temperature {
     private long t = 0;
     private double bt = 0; // smoker temp
@@ -10,54 +16,6 @@ public class Temperature {
     private double f = 0; // fan
     private double bs = 0; // smokertempset
 
-    public Temperature() {
-    }
 
-    public Temperature(long datetime, double bt, double mt, double f, double bs) {
-        this.t = datetime;
-        this.bt = bt;
-        this.mt = mt;
-        this.f = f;
-        this.bs = bs;
-    }
 
-    public long getT() {
-        return t;
-    }
-
-    public void setT(long t) {
-        this.t = t;
-    }
-
-    public double getBt() {
-        return bt;
-    }
-
-    public void setBt(double bt) {
-        this.bt = bt;
-    }
-
-    public double getMt() {
-        return mt;
-    }
-
-    public void setMt(double mt) {
-        this.mt = mt;
-    }
-
-    public double getF() {
-        return f;
-    }
-
-    public void setF(double f) {
-        this.f = f;
-    }
-
-    public double getBs() {
-        return bs;
-    }
-
-    public void setBs(double bs) {
-        this.bs = bs;
-    }
 }
